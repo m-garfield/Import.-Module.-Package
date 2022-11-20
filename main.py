6c54a347963c8257e4aecfb044c7775525d9ff7b
m-garfield <m-garfield@rambler.ru>
2022-11-20 16:57:45 +0300

    init_commit4

--- main.py
+++ main.py
@@ -2,11 +2,10 @@
 from aplication import salary
 from aplication.db import people
 import datetime
-
+import matplotlib
 def main():
     salary.calculate_salary()
     people.get_employees()
     print(datetime.date.today())
-
 if __name__ == '__main__':
     main()
